8efab7b44902c254ad2548dc173e507a5895b40d
NehaaBalotia <nehabalotia.clg@gmail.com>
2025-07-26 20:33:21 +0530

    Cleaned up code

--- scripts/preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  login: (credentials) => ipcRenderer.invoke('login', credentials),
-});
